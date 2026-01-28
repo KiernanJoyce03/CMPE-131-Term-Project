261bab2d672785435fcdbdc5e01f3522c25c0685
Kiernan <kiernanmj007@gmail.com>
2026-01-27 20:01:00 -0800

    cardContainer file initialized

---
+++ src/components/CardContainer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Card, CardContent } from "@/components/ui/card"
+
+
+function CardContainer() {
+  return (
+    <div>
+
+
+    </div>
+  )
+}
+
+export default CardContainer
