5c325ec6967c199da192a6f5fba819e4fe49810a
Kiernan <kiernanmj007@gmail.com>
2026-04-27 19:45:16 -0700

    api call setup in book page

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function CardContainer({ book }) {
   const thumbnail = book?.coverUrl
   const title = book?.title
+  const navigate = useNavigate()
 
   return (
-    <div className='relative w-full aspect-2/3 rounded-lg overflow-visible cursor-pointer
+
+    <div
+      onClick={() => navigate(`/home/book/${book.id.replace('/works/', '')}`)}
+      className='relative w-full aspect-2/3 rounded-lg overflow-visible cursor-pointer
       transition-all duration-300 ease-out
       hover:-translate-y-2 hover:translate-x-1
       hover:scale-105
@@ -23,6 +28,8 @@ function CardContainer({ book }) {
         </div>
       )}
     </div>
+
+
   )
 }
 
--- src/pages/bookPage/BookPage.jsx
+++ src/pages/bookPage/BookPage.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React , {useEffect} from 'react'
+import { useParams } from 'react-router-dom'
 
 function BookPage() {
+  const { id } = useParams();
+
+  useEffect(() => {
+    // Fetch book details using the id
+    const fetchBookDetails = async () => {
+      try {
+        const res = await fetch(`/api/books/work/${id}`)
+        const data = await res.json()
+        console.log(data) // For now, just log the data
+      } catch (err) {
+        console.error('Failed to fetch book details:', err)
+      }
+    }
+
+    fetchBookDetails()
+  }, [id])
+
   return (
-    <div>BookPage</div>
+    <div>
+      
+
+    </div>
   )
 }
 
